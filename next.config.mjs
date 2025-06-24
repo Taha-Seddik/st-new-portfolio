bec107f36d7d3675eaf70815065f5fea47e1d368
taha seddik <taha.seddik1992@gmail.com>
2025-06-24 08:00:57 +0400

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/st-new-portfolio" : "",
+  assetPrefix: isProd ? "/st-new-portfolio/" : "",
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
