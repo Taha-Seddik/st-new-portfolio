a71084f734536c351ba67ec7eb3caf40e635b92c
taha seddik <taha.seddik1992@gmail.com>
2025-06-24 22:24:21 +0400

    removed all

--- src/layout/AppLogo.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useIsUpMd } from "@/utils/responsive.utils";
-
-export const AppLogo: React.FC<{}> = ({}) => {
-  const isOnMd = useIsUpMd();
-  if (!isOnMd) return null;
-  return <img src="/tahaLogo.png" alt="logo" width={30} height={30} />;
-};
